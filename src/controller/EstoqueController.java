460251adeaaff051483d9de9e038719e398e8d0e
IzabelleSouza <izabellepsouzaa@gmail.com>
2025-05-22 13:49:48 -0300

    feat: implementar camada do controlador (Controller) – Produtos, Estoque e Fornecedores
    - Criadas classes ProdutoController, EstoqueController e FornecedorController
    - Métodos expostos: adicionar, listar, buscar por nome ou ID
    - Controllers conectados com os respectivos services
    - Operações baseadas nas histórias de negócio e critérios definidos

--- src/controller/EstoqueController.java
+++ src/controller/EstoqueController.java
@@ -1,4 +1,14 @@
 package controller;
 
 public class EstoqueController {
+    private EstoqueService estoqueService = new EstoqueService();
+
+    public void atualizarEstoque(int produtoId, int novaQuantidade) {
+        estoqueService.atualizarEstoque(produtoId, novaQuantidade);
+    }
+
+    public void consultarQuantidade(int produtoId) {
+        int quantidade = estoqueService.consultarQuantidadeProduto(produtoId);
+        System.out.println("Quantidade atual do produto " + produtoId + ": " + quantidade);
+    }
 }
--- src/controller/FornecedorController.java
+++ src/controller/FornecedorController.java
@@ -1,4 +1,23 @@
 package controller;
 
+import service.FornecedorService;
+import model.Fornecedor;
+
 public class FornecedorController {
+    private FornecedorService fornecedorService = new FornecedorService();
+
+    public void adicionarFornecedor(String nome, String cnpj) {
+        fornecedorService.adicionarFornecedor(nome, cnpj);
+    }
+
+    public void listarFornecedores() {
+        for (Fornecedor f : fornecedorService.listarFornecedores()) {
+            System.out.println(f);
+        }
+    }
+
+    public void buscarFornecedorPorCnpj(String cnpj) {
+        Fornecedor f = fornecedorService.buscarFornecedorPorCnpj(cnpj);
+        System.out.println(f != null ? f : "Fornecedor não encontrado.");
+    }
 }

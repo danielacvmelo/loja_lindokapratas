a53b81a745e6c9c5c1b7443c5a26a180b70008e0
danielacvmelo <daniela.cvmelo@gmail.com>
2025-05-23 15:33:27 -0300

    Subindo Fornecedor e Pedido corrigidos para a pasta Model

--- src/model/Fornecedor.java
+++ src/model/Fornecedor.java
@@ -2,7 +2,43 @@ package model;
 
 public class Fornecedor extends Pessoa {
 
-    private String
+    private Long id;
+    private String cnpj;
+    private String endereco;
+    private String descricao;
+    private String email;
+    private String telefone;
 
+    public Fornecedor(Long id, String cnpj, String descricao) {
+        this.id = id;
+        this.cnpj = cnpj;
+        this.descricao = descricao;
+        setTelefone(telefone);
+        setEmail(email);
+        setSenha(endereco);
+    }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
--- src/model/Pedido.java
+++ src/model/Pedido.java
@@ -1,4 +1,47 @@
 package model;
 
 public class Pedido {
+    private double valor;
+    private String data;
+    private Long id;
+    private double desconto;
+
+    public Pedido(double valor, String data, Long id, double desconto) {
+        this.valor = valor;
+        this.data = data;
+        this.id = id;
+        this.desconto = desconto;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(double desconto) {
+        this.desconto = desconto;
+    }
 }

d4a175cdada90835d6c21ebd589156520f2ceb8f
alvinopf <alvinopf@yahoo.com.br>
2025-05-25 22:04:42 -0300

    Subindo as Controllers Cliente, FuncionÂario e Pedido.

--- src/controller/ClienteController.java
+++ src/controller/ClienteController.java
@@ -1,4 +1,31 @@
 package controller;
 
+import model.Cliente;
+import service.ClienteService;
+import java.util.List;
+
 public class ClienteController {
+
+    private ClienteService clienteService = new ClienteService();
+
+    public void adicionarCliente(Long id, String nome, int idade, String endereco, String sexo, String cpf, String telefone, String email, String senha) {
+        Cliente cliente = new Cliente(id, nome, idade, endereco, sexo, cpf, telefone, email, senha);
+        clienteService.adicionarCliente(cliente);
+    }
+
+    public List<Cliente> listarClientes() {
+        return clienteService.listarClientes();
+    }
+
+    public Cliente buscarClientePorId(Long id) {
+        return clienteService.buscarPorId(id);
+    }
+
+    public Cliente buscarClientePorCpf(String cpf) {
+        return clienteService.buscarClientePorCpf(cpf);
+    }
+
+    public boolean removerCliente(Long id) {
+        return clienteService.removerCliente(id);
+    }
 }
--- src/controller/PedidoController.java
+++ src/controller/PedidoController.java
@@ -1,4 +1,37 @@
 package controller;
 
+import model.Pedido;
+import model.Produto;
+import service.PedidoService;
+
+import java.util.List;
+
 public class PedidoController {
+
+    private PedidoService pedidoService = new PedidoService();
+
+    public void adicionarPedido(double valor, String data, Long id, double desconto) {
+        Pedido pedido = new Pedido(valor, data, id, desconto);
+        pedidoService.adicionarPedido(pedido);
+    }
+
+    public List<Pedido> listarPedidos() {
+        return pedidoService.listarPedidos();
+    }
+
+    public Pedido buscarPedidoPorId(Long id) {
+        return pedidoService.buscarPorId(id);
+    }
+
+    public boolean removerPedido(Long id) {
+        return pedidoService.removerPedido(id);
+    }
+
+    public void adicionarProdutoAoPedido(Long pedidoId, Produto produto, int quantidade) {
+        pedidoService.adicionarProdutoAoPedido(pedidoId, produto, quantidade);
+    }
+
+    public void atualizarValorPedido(Long idPedido, double novoValorTotal, double novoDesconto) {
+        pedidoService.atualizarValorPedido(idPedido, novoValorTotal, novoDesconto);
+    }
 }
